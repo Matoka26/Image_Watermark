dada02c29c7e462da78bc057aa7dbedf87178a83
Mihai <mihail_danut24@yahoo.com>
2025-01-16 13:03:12 +0200

    added experiments and refactored directories

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from utils import encrypt as en
 import numpy as np
 import cv2
 from utils import watermark_image as wi
+import skimage
+
+import mahotas
 
 figures_dir = './figures'
 outputs_dir = './outputs'
@@ -19,14 +22,10 @@ if __name__ == '__main__':
     if not os.path.isdir(outputs_dir):
         os.makedirs(outputs_dir, exist_ok=True)
 
-    host_img = cv2.imread('lena.png')
-    watermark = cv2.imread('watermark.png', cv2.IMREAD_GRAYSCALE)
-
+    host_img = cv2.imread('hosts/cat.jpg', cv2.IMREAD_COLOR)
+    host_img = cv2.resize(host_img, (800, 800), interpolation = cv2.INTER_AREA)
+    watermark = cv2.imread('watermarks/catface.jpg', cv2.IMREAD_GRAYSCALE)
     new_image = wi.watermark_iamge(host_img, watermark)
     new_image = np.uint8(new_image)
 
-    cv2.imwrite(f'./{outputs_dir}/watermarked_lana.png', new_image)
-    cv2.imshow('Image', new_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
+    cv2.imwrite(f'{outputs_dir}/cat_catface.png', new_image)
